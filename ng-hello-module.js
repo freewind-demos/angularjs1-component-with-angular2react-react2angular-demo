29f4d19e7bec12d8c965379ccf0047316a096452
freewind <nowindlee@gmail.com>
2019-01-10 22:24:50 +0800

    remove the use of watch

--- ng-hello-module.js
+++ ng-hello-module.js
@@ -6,25 +6,28 @@ import ReactHello from './react-hello.jsx';
 const ngHelloModule = angular.module('helloModule', []);
 
 ngHelloModule.directive('helloDirective', function () {
-    return {
-        controller: function ($scope) {
-            $scope.message = 'Hello';
-        },
-        template: `<div>
-<div>Angular: <input type="text" ng-model="message"></div>
+  return {
+    controller: function ($scope) {
+      $scope.message = 'Hello';
+    },
+    template: `<div>
+<div>Angular: <input type="text" ng-model="message" ng-change="onMessageChange(message)"></div>
 <div class="react-app"></div>
 </div>`,
-        link: function ($scope, $element, $attrs) {
-            const reactRoot = $element[0].querySelector('.react-app');
+    link: function ($scope, $element, $attrs) {
+      function renderReact(message) {
+        console.log('> renderReact')
+        const reactRoot = $element[0].querySelector('.react-app');
+        ReactDOM.render(<ReactHello message={message} onMessageChange={(message) => {
+          console.log('message:', message)
+          $scope.message = message;
+          $scope.$apply();
+          renderReact($scope.message);
+        }}/>, reactRoot);
+      }
 
-            function onMessageChange(message) {
-                $scope.message = message;
-                $scope.$apply();
-            }
-
-            $scope.$watch('message', (message) => {
-                ReactDOM.render(<ReactHello message={message} onMessageChange={onMessageChange}/>, reactRoot);
-            })
-        }
+      renderReact($scope.message);
+      $scope.onMessageChange = (message) => renderReact(message)
     }
+  }
 });
