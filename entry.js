7bed9892a763a1463bd7a5717d990a89e44867b8
freewind <nowindlee@gmail.com>
2019-01-15 21:32:12 +0800

    demo

--- entry.js
+++ entry.js
@@ -1,4 +1,5 @@
 import angular from 'angular';
 import './ng-hello-module';
 
-const entry = angular.module('app', ['helloModule']);
+angular.module('app', ['helloModule']);
+
--- ng-hello-module.js
+++ ng-hello-module.js
@@ -1,26 +1,47 @@
 import angular from 'angular';
 import React from 'react';
-import ReactDOM from 'react-dom';
-import ReactHello from './react-hello.jsx';
 
 const {react2angular} = require('react2angular');
+const {angular2react} = require('angular2react');
 
-const ngHelloModule = angular.module('helloModule', []);
+let $injector;
+const ngHelloModule = angular.module('helloModule', [])
+  .run(['$injector', function (_$injector) {
+    $injector = _$injector
+  }]);
+// trigger the `.run()` above to initialize `$injector`
+angular.bootstrap(document.createElement('div'), [ngHelloModule.name])
 
-ngHelloModule.component('reactHelloComponent', react2angular(ReactHello, ['message', 'onMessageChange']))
+console.log('$injector:', $injector);
 
-ngHelloModule.directive('helloDirective', function () {
-  return {
-    controller: function ($scope) {
-      $scope.message = 'Hello';
-      $scope.onReactMessageChange = (message) => {
-        $scope.message = message;
-        $scope.$apply();
-      }
-    },
-    template: `<div>
-<div>Angular: <input type="text" ng-model="message" ></div>
-<react-hello-component message="message" on-message-change="onReactMessageChange">
-</div>`
+export const HelloComponent = {
+  template: `<div>Angular: <input type="text" ng-model="$ctrl.message" ng-change="$ctrl.onMessageChange($ctrl.message)">{{message}}</div>`,
+  bindings: {
+    message: '<',
+    onMessageChange: '<'
+  }
+};
+
+
+// ngHelloModule.component('helloComponent', AngularAgainHelloComponent)
+ngHelloModule.controller('HelloController', ($scope) => {
+  $scope.message = 'hello!';
+  $scope.onMessageChange = (message) => {
+    console.log(`Message changed to ${message}`)
   }
 });
+
+
+ngHelloModule.component('helloComponent', HelloComponent)
+
+const ReactHelloComponent = angular2react('helloComponent', HelloComponent, $injector)
+const AngularAgainHelloComponent = react2angular(ReactHelloComponent, ['message', 'onMessageChange']);
+
+console.log(AngularAgainHelloComponent)
+
+ngHelloModule.component('helloAgainComponent', AngularAgainHelloComponent)
+
+export default ngHelloModule;
+
+
+
